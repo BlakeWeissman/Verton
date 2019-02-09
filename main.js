05960099c9e0dd9bfad3f0613708f51676c04789
Blake Weissman <email@blakeweissman.com>
2019-02-08 23:08:42 -0500

    Added dev mode and removed MenuBar

--- main.js
+++ main.js
@@ -1,20 +1,50 @@
 const { app, BrowserWindow } = require('electron')
+const { globalShortcut } = require('electron')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+//SET FALSE FOR PUBLIC BUILDS
+let devMode = true
+
 let win
+let menubarToBeEnabled = true
 
 function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  //Create the browser window
+  win = new BrowserWindow({ width: 545, height: 360 })
 
-  // and load the index.html of the app.
+  //Load the index.html of the app
   win.loadFile('index.html')
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+  //If enabled, let user know DevMode is enabled
+  if (devMode === true) {
+    console.log("Notice: DevMode is enabled. \nKeyboard Shortcuts: \nCTRL/CMD+D: Open DevTools")
+  }
 
-  // Emitted when the window is closed.
+  //Open DevTools Using CTRL/CMD+D
+  globalShortcut.register('CommandOrControl+D', () => {
+    if (devMode === true) {
+      win.webContents.openDevTools()
+      console.log("Notice: DevTools opened.")
+    }
+  })
+
+  //Disable MenuBar by default
+  win.setMenu(null)
+
+  //Change MenuBar status using CTRL/CMD+M  
+  /*globalShortcut.register('CommandOrControl+M', () => {
+    if (devMode === true) {
+      if (menubarToBeEnabled === true) {
+        win.setMenu(menubar)
+        console.log("Notice: MenuBar enabled.")
+      }
+      else if (menubarToBeEnabled === false) {
+        win.setMenu(null)
+        console.log("Notice: MenuBar disabled.")
+      }
+    }
+  })*/
+
+  //When the window is closed
   win.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
@@ -23,9 +53,7 @@ function createWindow () {
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+//Create the window
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
